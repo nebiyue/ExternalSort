ab241ac31f06eaa668649283ad1406e0ce5a359e
nebiyue <33880444+nebiyue@users.noreply.github.com>
2020-04-23 22:05:00 -0400

    structure ish

---
+++ ExternalSort/src/ExternalSort.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.LinkedList;
+import java.io.RandomAccessFile;
+
+/**
+ * On my honor:
+- I have not used source code obtained from another student,
+or any other unauthorized source, either modified or
+unmodified.
+
+- All source code and documentation used in my program is
+either my original work, or was derived by me from the
+source code published in the textbook for this course.
+
+- I have not discussed coding details about this project with
+anyone other than my partner (in the case of a joint
+submission), instructor, ACM/UPE tutors or the TAs assigned
+to this course. I understand that I may discuss the concepts
+of this program with other students, and that another student
+may help me debug my program so long as neither of us writes
+anything during the discussion or modifies any computer file
+during the discussion. I have violated neither the spirit nor
+letter of this restriction.
+ */
+
+/**
+ * The main method class. Retrieves bytes from file in the form of input byte array
+ *
+ * @author Ananiya Admassu (ananiya0)
+ * @author Nebiyu Elias (nebiyue)
+ * @version 04/23/2020
+ */
+
+public class ExternalSort {
+    private static byte[] input;
+    private static byte[] output;
+    private static Record[] heapArr; 
+     
+    
+}
